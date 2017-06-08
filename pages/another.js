7dd0634cf27fdd0d206aef70eb33a346509c6ef1
fatfisz <fatfisz@gmail.com>
2017-06-08 16:42:24 +0200

    Add getInitialProps

--- pages/another.js
+++ pages/another.js
@@ -3,14 +3,20 @@ import Head from 'next/head';
 import Layout from 'components/layout';
 
 
-export default function AnotherPage(props) {
+export default function AnotherPage({ from, ...props }) {
   return (
     <Layout {...props}>
       <Head>
         <title>Another - Next.js demo</title>
       </Head>
 
-      This is another page
+      This is another page from the {from}
     </Layout>
   );
 }
+
+AnotherPage.getInitialProps = ({ req }) => {
+  return {
+    from: req ? 'server' : 'client',
+  };
+};
