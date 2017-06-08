f9f2d4c51ff8136127952aa870e9339be37fdb24
fatfisz <fatfisz@gmail.com>
2017-06-08 16:32:38 +0200

    Style the index page

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,17 @@
-export default () => <div>Hello world</div>;
+import Head from 'next/head';
+
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>Index - Next.js demo</title>
+        <link rel="stylesheet" href="/static/bootstrap.min.css" />
+      </Head>
+
+      <main className="container my-5">
+        Hello world
+      </main>
+    </div>
+  );
+}
